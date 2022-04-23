68d3385d891e3fc59f37298d890831ce81064276
lor-rainemanalo <lorrainemanalo1026@gmail.com>
2022-04-23 19:46:10 +0800

    remove unused cars and add alt for img

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Typography } from "@mui/material";
+import { Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import Logo from "../images/logo.png";
 
@@ -11,7 +11,7 @@ export default function ErrorPage() {
       <div className="error">
         <p className="error-text">
           4
-          <img src={Logo} height="115" className="App-logo" />4
+          <img src={Logo} height="115" className="App-logo" alt="logo" />4
         </p>
         <p className="error-subtitle">Page Not Found</p>
         <Button variant="contained" onClick={() => navigate("/")}>
